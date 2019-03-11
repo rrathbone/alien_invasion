11746223b49d196874a50454a1832ec859990b71
Rachelle Rathbone <rachelle@getmigo.com>
2019-03-10 21:02:54 -0700

    add ship class

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alien_invasion
+2D game to shoot down an alien invasion fleet. Project courtesy of Eric Matthes and the book _Python Crash Course_.
--- alien_invasion.py
+++ alien_invasion.py
@@ -2,18 +2,18 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height)
-    )
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Set background color
-    bg_color = (230, 230, 230)
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
@@ -24,6 +24,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
